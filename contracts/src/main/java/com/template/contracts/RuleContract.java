3565ebc06ca2136bd648826fb64d26e8e9e99253
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-07-05 13:42:30 +0200

    Add check for maximally one associated regulation.

--- contracts/src/main/java/com/template/contracts/RuleContract.java
+++ contracts/src/main/java/com/template/contracts/RuleContract.java
@@ -1,11 +1,13 @@
 package com.template.contracts;
 
+import com.template.states.Regulation;
 import com.template.states.Rule;
 import net.corda.core.contracts.CommandData;
 import net.corda.core.contracts.Contract;
 import net.corda.core.transactions.LedgerTransaction;
 
 import java.util.Objects;
+import java.util.stream.Stream;
 
 import static net.corda.core.contracts.ContractsDSL.requireThat;
 
@@ -26,22 +28,30 @@ public class RuleContract implements Contract {
         //final CommandWithParties<Commands> command = requireSingleCommand(tx.getCommands(), Commands.class);
         final CommandData commandData = tx.getCommands().get(0).getValue();
 
-        if (commandData instanceof Commands.CreateRule) {
-            //Retrieve the output state of the transaction
-            Rule output = tx.outputsOfType(Rule.class).get(0);
+        //Retrieve the output state of the transaction
+        Rule output = tx.outputsOfType(Rule.class).get(0);
 
+        final Stream<Regulation> regulation = tx.referenceInputRefsOfType(Regulation.class).stream()
+                .map(it -> it.getState().getData())
+                // Only the expected issuer.
+                .filter(it -> it.getIssuer().equals(output.getIssuer()));
+
+
+        if (commandData instanceof Commands.CreateRule) {
             //No verification required!
             requireThat(require -> {
                 require.using("The rule is not empty", !Objects.equals(output.getName(), "") && !Objects.equals(output.getRuleSpecification(), ""));
+                require.using("A rule is associated with exactly one regulation.", regulation.count() == 1);
                 return null;
             });
+
         } else if (commandData instanceof Commands.UpdateRule) {
-            Rule output = tx.outputsOfType(Rule.class).get(0);
             Rule input = tx.inputsOfType(Rule.class).get(0);
 
             requireThat(require -> {
                 require.using("The transaction is only allowed to modify the input rule", output.getLinearId().equals(input.getLinearId()));
                 require.using("The rule is not empty", !Objects.equals(output.getName(), "") && !Objects.equals(output.getRuleSpecification(), ""));
+                require.using("A rule is associated with exactly one regulation.", regulation.count() == 1);
                 return null;
             });
         }
