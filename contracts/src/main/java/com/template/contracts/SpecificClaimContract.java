daac23d911d702ff7904ecc8c2b14eefa552d28c
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-07-05 16:06:46 +0200

    Refactor SpecificClaim so that it includes relation to ClaimTemplate and other supporting SpecificClaims.

--- contracts/src/main/java/com/template/contracts/SpecificClaimContract.java
+++ contracts/src/main/java/com/template/contracts/SpecificClaimContract.java
@@ -1,9 +1,14 @@
 package com.template.contracts;
 
+import com.template.states.ClaimTemplate;
 import com.template.states.SpecificClaim;
 import net.corda.core.contracts.CommandData;
 import net.corda.core.contracts.Contract;
+import net.corda.core.contracts.StateAndRef;
 import net.corda.core.transactions.LedgerTransaction;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static net.corda.core.contracts.ContractsDSL.requireThat;
 
@@ -24,11 +29,16 @@ public class SpecificClaimContract implements Contract {
         //final CommandWithParties<Commands> command = requireSingleCommand(tx.getCommands(), Commands.class);
         final CommandData commandData = tx.getCommands().get(0).getValue();
 
-        if (commandData instanceof Commands.SendClaim) {
-            //Retrieve the output state of the transaction
-            SpecificClaim output = tx.outputsOfType(SpecificClaim.class).get(0);
+        // Retrieve the output state of the transaction
+        SpecificClaim output = tx.outputsOfType(SpecificClaim.class).get(0);
 
-            //Using Corda DSL function requireThat to replicate conditions-checks
+        StateAndRef<ClaimTemplate> claimTemplate = output.getClaimTemplate().resolve(tx);
+        List<StateAndRef<SpecificClaim>> supportingClaims = output.getSupportingClaims().stream().map(claimTemplateLinearPointer -> claimTemplateLinearPointer.resolve(tx)).collect(Collectors.toList());
+
+
+        if (commandData instanceof Commands.CreateClaim) {
+
+            // Using Corda DSL function requireThat to replicate conditions-checks
             requireThat(require -> {
                 require.using("The transaction should have exactly one Claim as output", tx.getOutputs().size() == 1);
                 require.using("The output of Claim must have a hash value", !output.getHashValue().equals(""));
@@ -40,6 +50,13 @@ public class SpecificClaimContract implements Contract {
     // Used to indicate the transaction's intent.
     public interface Commands extends CommandData {
         //In our hello-world app, We will only have one command.
-        class SendClaim implements Commands {}
+        class CreateClaim implements Commands {
+        }
+
+        class AddEvidence implements Commands {
+        }
+
+        class ResubmitClaim implements Commands {
+        }
     }
 }
--- contracts/src/main/java/com/template/states/SpecificClaim.java
+++ contracts/src/main/java/com/template/states/SpecificClaim.java
@@ -2,6 +2,7 @@ package com.template.states;
 
 import com.template.contracts.SpecificClaimContract;
 import net.corda.core.contracts.BelongsToContract;
+import net.corda.core.contracts.LinearPointer;
 import net.corda.core.contracts.LinearState;
 import net.corda.core.contracts.UniqueIdentifier;
 import net.corda.core.identity.AbstractParty;
@@ -11,6 +12,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 // *********
 // * State *
@@ -31,24 +33,45 @@ public class SpecificClaim implements LinearState {
     private final Party supervisorAuthority;
     private final List<AbstractParty> participants;
 
+    // A reference to the claim template that is implemented by this specific claim.
+    private final LinearPointer<ClaimTemplate> claimTemplate;
+
+    // References to other specific claims which can be used as evidence references when this claim is to be proven.
+    private final List<LinearPointer<SpecificClaim>> supportingClaims;
+
+
     /* Constructor of your Corda state */
     @ConstructorForDeserialization
-    public SpecificClaim(String hashValue, @NotNull Party financialServiceProvider, @NotNull Party supervisorAuthority) {
+    public SpecificClaim(String hashValue, @NotNull Party financialServiceProvider, @NotNull Party supervisorAuthority, UniqueIdentifier claimTemplateLinearId, List<UniqueIdentifier> supportingClaimsLinearIds) {
         this.linearId = new UniqueIdentifier();
         this.financialServiceProvider = financialServiceProvider;
         this.supervisorAuthority = supervisorAuthority;
         this.hashValue = hashValue;
+        this.claimTemplate = new LinearPointer<>(claimTemplateLinearId, ClaimTemplate.class);
+        this.supportingClaims = supportingClaimsLinearIds.stream().map(claimLinearId -> {
+            return new LinearPointer<>(claimLinearId, SpecificClaim.class);
+        }).collect(Collectors.toList());
+
+
         this.participants = new ArrayList<>();
         this.participants.add(financialServiceProvider);
         this.participants.add(supervisorAuthority);
     }
 
-    public SpecificClaim(@NotNull UniqueIdentifier linearId, String hashValue, @NotNull Party financialServiceProvider, @NotNull Party supervisorAuthority) {
+    public SpecificClaim(@NotNull UniqueIdentifier linearId, String hashValue, @NotNull Party financialServiceProvider, @NotNull Party supervisorAuthority, UniqueIdentifier claimTemplateLinearId, List<UniqueIdentifier> supportedClaimsLinearIds) {
         this.linearId = linearId;
         this.hashValue = hashValue;
         this.financialServiceProvider = financialServiceProvider;
         this.supervisorAuthority = supervisorAuthority;
+        this.claimTemplate = new LinearPointer<>(claimTemplateLinearId, ClaimTemplate.class);
+        this.supportingClaims = supportedClaimsLinearIds.stream().map(claimLinearId -> {
+            return new LinearPointer<>(claimLinearId, SpecificClaim.class);
+        }).collect(Collectors.toList());
+
         this.participants = new ArrayList<>();
+        this.participants.add(financialServiceProvider);
+        this.participants.add(supervisorAuthority);
+
     }
 
     @NotNull
@@ -63,11 +86,6 @@ public class SpecificClaim implements LinearState {
     }
 
     @NotNull
-    public Party getFinancialServiceProiver() {
-        return financialServiceProvider;
-    }
-
-    @NotNull
     public Party getSupervisorAuthority() {
         return supervisorAuthority;
     }
@@ -76,5 +94,18 @@ public class SpecificClaim implements LinearState {
     @Override
     public UniqueIdentifier getLinearId() {
         return this.linearId;
+    }
+
+    @NotNull
+    public Party getFinancialServiceProvider() {
+        return financialServiceProvider;
+    }
+
+    public LinearPointer<ClaimTemplate> getClaimTemplate() {
+        return claimTemplate;
+    }
+
+    public List<LinearPointer<SpecificClaim>> getSupportingClaims() {
+        return supportingClaims;
     }
 }
