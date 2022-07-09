0aa0c030e8d3aade79f0712123dfed9125c38d06
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-07-09 19:36:30 +0200

    Create regulation endpoints with custom DTO for regulation creation

--- clients/src/main/java/com/compliance/supervisoryauthority/controllers/RegulationController.java
+++ clients/src/main/java/com/compliance/supervisoryauthority/controllers/RegulationController.java
@@ -1,25 +1,25 @@
 package com.compliance.supervisoryauthority.controllers;
 
-import com.compliance.flows.CreateClaimTemplate;
 import com.compliance.flows.CreateRegulation;
-import com.compliance.flows.CreateRule;
-import com.compliance.states.ClaimTemplate;
+import com.compliance.flows.UpdateRegulation;
 import com.compliance.states.Regulation;
-import com.compliance.states.Rule;
 import com.compliance.supervisoryauthority.NodeRPCConnection;
-import net.corda.core.contracts.StateAndRef;
+import com.compliance.supervisoryauthority.models.RegulationDTO;
 import net.corda.core.contracts.UniqueIdentifier;
 import net.corda.core.identity.CordaX500Name;
 import net.corda.core.messaging.CordaRPCOps;
+import net.corda.core.node.services.Vault;
+import net.corda.core.node.services.vault.QueryCriteria;
 import net.corda.core.transactions.SignedTransaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -27,7 +27,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
  * Define your API endpoints here.
  */
 @RestController
-@RequestMapping("/regulation") // The paths for HTTP requests are relative to this base path.
+@RequestMapping("/regulations") // The paths for HTTP requests are relative to this base path.
 public class RegulationController {
     private final CordaRPCOps proxy;
     private final CordaX500Name me;
@@ -38,86 +38,78 @@ public class RegulationController {
         this.me = proxy.nodeInfo().getLegalIdentities().get(0).getName();
     }
 
-    @GetMapping(value = "/regulations", produces = APPLICATION_JSON_VALUE)
-    private List<StateAndRef<Regulation>> regulations() {
-        return proxy.vaultQuery(Regulation.class).getStates();
+    @GetMapping(value = "/", produces = APPLICATION_JSON_VALUE)
+    private List<Regulation> getAll() {
+        return proxy
+                .vaultQuery(Regulation.class)
+                .getStates()
+                .stream()
+                .map(
+                        regulationStateAndRef -> regulationStateAndRef.getState().getData()
+                )
+                .collect(Collectors.toList());
     }
 
-    @GetMapping(value = "/rules", produces = APPLICATION_JSON_VALUE)
-    private List<StateAndRef<Rule>> rules() {
-        return proxy.vaultQuery(Rule.class).getStates();
+    @GetMapping(value = "/{linearId}", produces = APPLICATION_JSON_VALUE)
+    private List<Regulation> getByLinearId(@PathVariable String linearId) {
+        QueryCriteria queryCriteria = new QueryCriteria.LinearStateQueryCriteria(null, Collections.singletonList(UniqueIdentifier.Companion.fromString(linearId)), Vault.StateStatus.ALL, Collections.singleton(Regulation.class));
+        return proxy
+                .vaultQueryByCriteria(queryCriteria, Regulation.class)
+                .getStates()
+                .stream()
+                .map(
+                        regulationStateAndRef -> regulationStateAndRef.getState().getData()
+                )
+                .collect(Collectors.toList());
     }
 
-    @GetMapping(value = "/claimtemplates", produces = APPLICATION_JSON_VALUE)
-    private List<StateAndRef<ClaimTemplate>> claimTemplates() {
-        return proxy.vaultQuery(ClaimTemplate.class).getStates();
-    }
-
-    @GetMapping(value = "/bootstrapGraph", produces = APPLICATION_JSON_VALUE)
-    private void bootstrapGraph() {
-        // Issue the first regulation
-        logger.info("Bootstrapping regulation graph");
-
-        try {
-            logger.info("Creating regulation");
-            SignedTransaction regulation = proxy.startTrackedFlowDynamic(
-                    CreateRegulation.CreateRegulationInitiator.class,
-                    "MaRisk AT 7",
-                    "Provides a flexible and practical framework for structuring institutions' risk management [on the basis of Kreditwesengestz §25a and §25b]",
-                    "0.1",
-                    new Date()
+    @PutMapping(value = "/update")
+    private void updateRegulation(String linearId, String name, String description, String version, Date releaseDate) throws ExecutionException, InterruptedException {
+        UniqueIdentifier id = UniqueIdentifier.Companion.fromString(linearId);
+        QueryCriteria queryCriteria = new QueryCriteria.LinearStateQueryCriteria(null, Collections.singletonList(id), Vault.StateStatus.ALL, Collections.singleton(Regulation.class));
+        // Check if state with that linear ID exists
+        // Todo: Should throw custom exception if no regulation with the ID exists
+        if (!proxy.vaultQueryByCriteria(queryCriteria, Regulation.class).getStates().isEmpty()) {
+            // Call the update flow
+            SignedTransaction tx = proxy.startTrackedFlowDynamic(
+                    UpdateRegulation.UpdateRegulationInitiator.class,
+                    linearId,
+                    name,
+                    description,
+                    version,
+                    releaseDate
             ).getReturnValue().get();
-
-            // Get regulation linear ID
-            logger.info("Fetching regulation linear ID from vault");
-            UniqueIdentifier regulationLinearId = proxy
-                    .vaultQuery(Regulation.class)
-                    .getStates()
-                    .get(0)
-                    .getState()
-                    .getData()
-                    .getLinearId();
-
-            // Create sample rules
-            logger.info("Creating rule");
-            SignedTransaction rule = proxy.startTrackedFlowDynamic(
-                    CreateRule.CreateRuleInitiator.class,
-                    "MaRisk AT 7.2 p. 3",
-                    "The IT systems shall be tested before their first use and after any material changes and approved by both the responsible organisational unit staff and IT staff. To this end, a standard process of development, testing, approval and implementation in the production processes shall be established. The production and testing environments shall be segregated.",
-                    regulationLinearId
-            ).getReturnValue().get();
-
-            // Create claim templates
-            logger.info("Fetching rule linear ID from vault");
-            UniqueIdentifier ruleLinearId = proxy
-                    .vaultQuery(Rule.class)
-                    .getStates()
-                    .get(0)
-                    .getState()
-                    .getData()
-                    .getLinearId();
-
-            logger.info("Creating developer claim template");
-            SignedTransaction developersClaimTemplate = proxy.startTrackedFlowDynamic(
-                    CreateClaimTemplate.CreateClaimTemplateInitiator.class,
-                    "Developer Access",
-                    "Proof that Developers only accessed development/testing systems.",
-                    ruleLinearId
-            ).getReturnValue().get();
-
-            logger.info("Creating admin claim template");
-            SignedTransaction adminsClaimTemplate = proxy.startTrackedFlowDynamic(
-                    CreateClaimTemplate.CreateClaimTemplateInitiator.class,
-                    "MaRisk AT 7.2 p. 3",
-                    "Proof that IT admins only accessed production systems.",
-                    ruleLinearId
-            ).getReturnValue().get();
-
-        } catch (InterruptedException | ExecutionException e) {
-            logger.error("Failed bootstrapping regulation graph", e);
-            e.printStackTrace();
         }
-
-
     }
+
+    @PostMapping("/create")
+    private UniqueIdentifier createRegulation(@RequestBody RegulationDTO regulationDTO) throws ExecutionException, InterruptedException {
+        SignedTransaction tx = proxy.startTrackedFlowDynamic(
+                CreateRegulation.CreateRegulationInitiator.class,
+                regulationDTO.getName(),
+                regulationDTO.getDescription(),
+                regulationDTO.getVersion(),
+                new Date()
+        ).getReturnValue().get();
+
+        List<Regulation> regulations = proxy
+                .vaultQuery(Regulation.class)
+                .getStates()
+                .stream()
+                .map(
+                        regulationStateAndRef -> regulationStateAndRef.getState().getData())
+                .collect(Collectors.toList());
+
+        // Return regulation linear ID
+        return regulations
+                .stream()
+                .filter(
+                        reg -> reg.getName().equals(regulationDTO.getName()) && reg.getDescription().equals(regulationDTO.getDescription())
+                )
+                .collect(Collectors.toList())
+                .get(0)
+                .getLinearId();
+    }
+
+
 }
