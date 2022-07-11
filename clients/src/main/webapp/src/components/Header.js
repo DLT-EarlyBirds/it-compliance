854b45802a599ce70a4a7a3bdb4f165c813e1e87
Vigan Lladrovci <v.lladrovci@finway.de>
2022-07-11 12:21:10 +0200

    fix navigation header issue

--- clients/src/main/webapp/src/components/Header.js
+++ clients/src/main/webapp/src/components/Header.js
@@ -6,23 +6,31 @@ import {
   OneToOneOutlined,
   HomeOutlined,
 } from "@ant-design/icons";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-const Header = () => (
-  <Menu mode="horizontal" defaultSelectedKeys={["home"]}>
-    <Menu.Item key="home" icon={<HomeOutlined />}>
-      <Link to="/">Home</Link>
-    </Menu.Item>
-    <Menu.Item key="graph-regulation" icon={<RadarChartOutlined />}>
-      <Link to="/graph-regulation">Graph Regulation</Link>
-    </Menu.Item>
-    <Menu.Item key="regulation" icon={<OneToOneOutlined />}>
-      <Link to="/regulation">Regulations</Link>
-    </Menu.Item>
-    <Menu.Item key="claim-template" icon={<FileTextOutlined />}>
-      <Link to="/claim-template">Claim Templates</Link>
-    </Menu.Item>
-  </Menu>
-);
+const Header = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <Menu
+      mode="horizontal"
+      selectedKeys={[pathname]}
+      defaultSelectedKeys={["home"]}
+    >
+      <Menu.Item key="/home" icon={<HomeOutlined />}>
+        <Link to="/">Home</Link>
+      </Menu.Item>
+      <Menu.Item key="/graph-regulation" icon={<RadarChartOutlined />}>
+        <Link to="/graph-regulation">Graph Regulation</Link>
+      </Menu.Item>
+      <Menu.Item key="/regulation" icon={<OneToOneOutlined />}>
+        <Link to="/regulation">Regulations</Link>
+      </Menu.Item>
+      <Menu.Item key="/claim-template" icon={<FileTextOutlined />}>
+        <Link to="/claim-template">Claim Templates</Link>
+      </Menu.Item>
+    </Menu>
+  );
+};
 
 export default Header;
