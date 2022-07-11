5507a9e2226fb0785226accd4eacb645b9b3623f
Mohammad.Al-Zoubi <mohammad.al-zoubi@samdsolutions.de>
2022-07-11 20:25:53 +0200

    Corrected review feedback

--- contracts/src/main/java/com/compliance/states/ClaimTemplateSuggestion.java
+++ contracts/src/main/java/com/compliance/states/ClaimTemplateSuggestion.java
@@ -89,6 +89,9 @@ public class ClaimTemplateSuggestion implements LinearState {
         return issuer;
     }
 
+    public Party getSupervisoryAuthority() {
+        return supervisoryAuthority;
+    }
 
     @NotNull
     @Override
