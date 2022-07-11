182ba1819297bead889be7ea3f598629765e9c2f
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-07-11 16:58:06 +0200

    Create endpoints for claim templates

--- clients/src/main/java/com/compliance/supervisoryauthority/controllers/ClaimTemplateController.java
+++ clients/src/main/java/com/compliance/supervisoryauthority/controllers/ClaimTemplateController.java
@@ -1,17 +1,27 @@
 package com.compliance.supervisoryauthority.controllers;
 
 
+
+import com.compliance.flows.CreateClaimTemplate;
+import com.compliance.flows.UpdateClaimTemplate;
 import com.compliance.states.ClaimTemplate;
 import com.compliance.supervisoryauthority.NodeRPCConnection;
-import net.corda.core.contracts.StateAndRef;
+import com.compliance.supervisoryauthority.models.ClaimTemplateDTO;
+import net.corda.core.contracts.UniqueIdentifier;
 import net.corda.core.identity.CordaX500Name;
 import net.corda.core.messaging.CordaRPCOps;
+import net.corda.core.node.services.Vault;
+import net.corda.core.node.services.vault.QueryCriteria;
+import net.corda.core.transactions.SignedTransaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -31,7 +41,74 @@ public class ClaimTemplateController {
     }
 
     @GetMapping(value = "/", produces = APPLICATION_JSON_VALUE)
-    private List<StateAndRef<ClaimTemplate>> getAll() {
-        return proxy.vaultQuery(ClaimTemplate.class).getStates();
+    private List<ClaimTemplate> getAll() {
+        return proxy
+                .vaultQuery(ClaimTemplate.class)
+                .getStates()
+                .stream()
+                .map(
+                        claimTemplateStateAndRef -> claimTemplateStateAndRef.getState().getData()
+                )
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping(value = "/{linearId}", produces = APPLICATION_JSON_VALUE)
+    private List<ClaimTemplate> getByLinearId(@PathVariable String linearId) {
+        QueryCriteria queryCriteria = new QueryCriteria.LinearStateQueryCriteria(null, Collections.singletonList(UniqueIdentifier.Companion.fromString(linearId)), Vault.StateStatus.ALL, Collections.singleton(ClaimTemplate.class));
+        return proxy
+                .vaultQueryByCriteria(queryCriteria, ClaimTemplate.class)
+                .getStates()
+                .stream()
+                .map(
+                        claimTemplateStateAndRef -> claimTemplateStateAndRef.getState().getData()
+                )
+                .collect(Collectors.toList());
+    }
+
+    @PutMapping(value = "/")
+    private void update(@RequestBody ClaimTemplateDTO claimTemplateDTO) throws ExecutionException, InterruptedException {
+        UniqueIdentifier id = UniqueIdentifier.Companion.fromString(claimTemplateDTO.getLinearId());
+        QueryCriteria queryCriteria = new QueryCriteria.LinearStateQueryCriteria(null, Collections.singletonList(id), Vault.StateStatus.ALL, Collections.singleton(ClaimTemplate.class));
+        // Check if state with that linear ID exists
+        // Todo: Should throw custom exception if no regulation with the ID exists
+        if (!proxy.vaultQueryByCriteria(queryCriteria, ClaimTemplate.class).getStates().isEmpty()) {
+            // Call the update flow
+            SignedTransaction tx = proxy.startTrackedFlowDynamic(
+                    UpdateClaimTemplate.UpdateClaimTemplateInitiator.class,
+                    id,
+                    claimTemplateDTO.getName(),
+                    claimTemplateDTO.getTemplateDescription(),
+                    UniqueIdentifier.Companion.fromString(claimTemplateDTO.getRule())
+            ).getReturnValue().get();
+        }
+    }
+
+
+    @PostMapping("/")
+    private ClaimTemplate create(@RequestBody ClaimTemplateDTO claimTemplateDTO) throws ExecutionException, InterruptedException {
+        SignedTransaction tx = proxy.startTrackedFlowDynamic(
+                CreateClaimTemplate.CreateClaimTemplateInitiator.class,
+                claimTemplateDTO.getName(),
+                claimTemplateDTO.getTemplateDescription(),
+                UniqueIdentifier.Companion.fromString(claimTemplateDTO.getRule()),
+                new Date()
+        ).getReturnValue().get();
+
+        List<ClaimTemplate> claimTemplates = proxy
+                .vaultQuery(ClaimTemplate.class)
+                .getStates()
+                .stream()
+                .map(
+                        ruleStateAndRef -> ruleStateAndRef.getState().getData())
+                .collect(Collectors.toList());
+
+        // Return regulation linear ID
+        return claimTemplates
+                .stream()
+                .filter(
+                        claimTemplate -> claimTemplate.getName().equals(claimTemplateDTO.getName()) && claimTemplate.getTemplateDescription().equals(claimTemplateDTO.getTemplateDescription())
+                )
+                .collect(Collectors.toList())
+                .get(0);
     }
 }
