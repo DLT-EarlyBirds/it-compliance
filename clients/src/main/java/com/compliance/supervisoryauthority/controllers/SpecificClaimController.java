11ca13cb3d0100bba35355305df31f797bd89dca
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-07-11 16:58:33 +0200

    Create endpoints for specific claims

--- clients/src/main/java/com/compliance/supervisoryauthority/controllers/SpecificClaimController.java
+++ clients/src/main/java/com/compliance/supervisoryauthority/controllers/SpecificClaimController.java
@@ -1,25 +1,23 @@
 package com.compliance.supervisoryauthority.controllers;
 
-import com.compliance.flows.CreateClaimTemplate;
-import com.compliance.flows.CreateRegulation;
-import com.compliance.flows.CreateRule;
-import com.compliance.states.ClaimTemplate;
-import com.compliance.states.Regulation;
-import com.compliance.states.Rule;
+import com.compliance.states.SpecificClaim;
 import com.compliance.supervisoryauthority.NodeRPCConnection;
-import net.corda.core.contracts.StateAndRef;
 import net.corda.core.contracts.UniqueIdentifier;
 import net.corda.core.identity.CordaX500Name;
+import net.corda.core.identity.Party;
 import net.corda.core.messaging.CordaRPCOps;
-import net.corda.core.transactions.SignedTransaction;
+import net.corda.core.node.services.Vault;
+import net.corda.core.node.services.vault.QueryCriteria;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -38,4 +36,75 @@ public class SpecificClaimController {
         this.me = proxy.nodeInfo().getLegalIdentities().get(0).getName();
     }
 
+    @GetMapping(value = "/", produces = APPLICATION_JSON_VALUE)
+    private List<SpecificClaim> getAll() {
+        return proxy
+                .vaultQuery(SpecificClaim.class)
+                .getStates()
+                .stream()
+                .map(
+                        specificClaimStateAndRef -> specificClaimStateAndRef.getState().getData()
+                )
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping(value = "/{linearId}", produces = APPLICATION_JSON_VALUE)
+    private List<SpecificClaim> getByLinearId(@PathVariable String linearId) {
+        QueryCriteria queryCriteria = new QueryCriteria.LinearStateQueryCriteria(
+                null,
+                Collections.singletonList(UniqueIdentifier.Companion.fromString(linearId)),
+                Vault.StateStatus.ALL,
+                Collections.singleton(SpecificClaim.class)
+        );
+        return proxy
+                .vaultQueryByCriteria(queryCriteria, SpecificClaim.class)
+                .getStates()
+                .stream()
+                .map(
+                        claimTemplateStateAndRef -> claimTemplateStateAndRef.getState().getData()
+                )
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping(value = "/{name}/", produces = APPLICATION_JSON_VALUE)
+    private List<SpecificClaim> getAllForOrg(@PathVariable String name) {
+        if (!proxy.partiesFromName(name, true).isEmpty()) {
+            List<Party> issuer = new ArrayList<>(proxy.partiesFromName(name, true));
+            QueryCriteria criteria = new QueryCriteria.LinearStateQueryCriteria(
+                    issuer,
+                    null,
+                    Vault.StateStatus.ALL,
+                    Collections.singleton(SpecificClaim.class)
+            );
+            return proxy
+                    .vaultQueryByCriteria(criteria, SpecificClaim.class)
+                    .getStates()
+                    .stream()
+                    .map(
+                            specificClaimStateAndRef -> specificClaimStateAndRef.getState().getData()
+                    )
+                    .collect(Collectors.toList());
+        } else return new ArrayList<>();
+    }
+
+    @GetMapping(value = "/{name}/{linearId}", produces = APPLICATION_JSON_VALUE)
+    private List<SpecificClaim> getAllForOrg(@PathVariable String name, @PathVariable String linearId) {
+        if (!proxy.partiesFromName(name, true).isEmpty()) {
+            List<Party> issuer = new ArrayList<>(proxy.partiesFromName(name, true));
+            QueryCriteria criteria = new QueryCriteria.LinearStateQueryCriteria(
+                    issuer,
+                    Collections.singletonList(UniqueIdentifier.Companion.fromString(linearId)),
+                    Vault.StateStatus.ALL,
+                    Collections.singleton(SpecificClaim.class)
+            );
+            return proxy
+                    .vaultQueryByCriteria(criteria, SpecificClaim.class)
+                    .getStates()
+                    .stream()
+                    .map(
+                            specificClaimStateAndRef -> specificClaimStateAndRef.getState().getData()
+                    )
+                    .collect(Collectors.toList());
+        } else return new ArrayList<>();
+    }
 }
