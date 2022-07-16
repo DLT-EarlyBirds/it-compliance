68f350d33f6ca680ca3a0111da5f2445ec84bacc
Vigan Lladrovci <v.lladrovci@finway.de>
2022-07-16 18:19:08 +0200

    Add nodes & edges for the graph

--- clients/src/main/webapp/src/views/GraphRegulation.js
+++ clients/src/main/webapp/src/views/GraphRegulation.js
@@ -1,6 +1,32 @@
 import React from "react";
+import { useData } from "../contexts/DataContext";
 
 function GraphRegulation() {
+  const { regulations, rules, claimTemplates } = useData();
+
+  const graphNodes = [
+    ...regulations.map((regulation) => ({
+      id: regulation.linearId.id,
+      name: regulation.name,
+    })),
+    ...rules.map((rule) => ({ id: rule.linearId.id, name: rule.name })),
+    ...claimTemplates.map((claimTemplate) => ({
+      id: claimTemplate.linearId.id,
+      name: claimTemplate.name,
+    })),
+  ];
+
+  const graphEdges = [
+    ...rules.map((rule) => ({
+      source: rule.parentRegulation,
+      target: rule.id,
+    })),
+    ...claimTemplates.map((claimTemplate) => ({
+      source: claimTemplate.rule,
+      target: claimTemplate.linearId.id,
+    })),
+  ];
+
   return <h1>Graph Regulation</h1>;
 }
 
