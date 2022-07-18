85e06f03d5a68e83047b4d5692684c56a986c388
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-07-18 15:47:55 +0200

    Create all endpoints

---
+++ clients/src/main/webapp/src/services/ClaimTemplate.service.ts
@@ -0,0 +1,54 @@
+import {AxiosInstance} from "axios";
+import {ClaimTemplateDTO, ClaimTemplateSuggestionDTO} from "../models";
+
+const MODEL = "claimtemplates";
+
+const ClaimTemplateService = {
+  getAll: async (axios: AxiosInstance) => {
+    const { data } = await axios.get(`/${MODEL}/`);
+    return data;
+  },
+  getByLinearId: async (axios: AxiosInstance, linearId: string) => {
+    const { data } = await axios.get(`/${MODEL}/${linearId}`);
+    return data;
+  },
+  update: async (axios: AxiosInstance, claimTemplate: ClaimTemplateDTO) => {
+    const { data } = await axios.put(`/${MODEL}/`, claimTemplate);
+    return data;
+  },
+  create: async (axios: AxiosInstance, claimTemplate: ClaimTemplateDTO) => {
+    const { data } = await axios.post(`/${MODEL}/`, claimTemplate);
+    return data;
+  },
+  createSuggestion: async (axios: AxiosInstance, claimTemplateSuggestion: ClaimTemplateSuggestionDTO) => {
+    const { data } = await axios.post(
+      `${MODEL}/suggestions`,
+      claimTemplateSuggestion
+    );
+    return data;
+  },
+  getSuggestionByLinearId: async (axios: AxiosInstance, linearId: string) => {
+    const { data } = await axios.get(`/${MODEL}/suggestions/${linearId}`);
+    return data;
+  },
+  getSuggestionBs: async (axios: AxiosInstance) => {
+    const { data } = await axios.get(`/${MODEL}/suggestions/`);
+    return data;
+  },
+  acceptSuggestion: async (axios: AxiosInstance, linearId: string) => {
+    const { data } = await axios.post(
+        `${MODEL}/suggestions/${linearId}`
+    );
+    return data;
+  },
+  rejectSuggestion: async (axios: AxiosInstance, linearId: string) => {
+    const { data } = await axios.delete(
+        `${MODEL}/suggestions/${linearId}`
+    );
+    return data;
+  },
+
+
+};
+
+export default ClaimTemplateService;
---
+++ clients/src/main/webapp/src/services/Network.service.ts
@@ -0,0 +1,41 @@
+import {AxiosInstance} from "axios";
+import {RegulationDTO, SpecificClaimDTO} from "../models";
+
+const MODEL = "network";
+
+const NetworkService = {
+    servertime: async (axios: AxiosInstance) => {
+        const {data} = await axios.get(`/${MODEL}/servertime`);
+        return data;
+    },
+    addresses: async (axios: AxiosInstance) => {
+        const {data} = await axios.get(`/${MODEL}/addresses`);
+        return data;
+    },
+    platformversion: async (axios: AxiosInstance) => {
+        const {data} = await axios.get(`/${MODEL}/platformversion`);
+        return data;
+    },
+    identities: async (axios: AxiosInstance) => {
+        const {data} = await axios.get(`/${MODEL}/identities`);
+        return data;
+    },
+    peers: async (axios: AxiosInstance) => {
+        const {data} = await axios.get(`/${MODEL}/peers`);
+        return data;
+    },
+    notaries: async (axios: AxiosInstance) => {
+        const {data} = await axios.get(`/${MODEL}/notaries`);
+        return data;
+    },
+    flows: async (axios: AxiosInstance) => {
+        const {data} = await axios.get(`/${MODEL}/flows`);
+        return data;
+    },
+    bootstrapGraph: async (axios: AxiosInstance) => {
+        const {data} = await axios.post(`/${MODEL}/bootstrapGraph`);
+        return data;
+    },
+};
+
+export default NetworkService;
