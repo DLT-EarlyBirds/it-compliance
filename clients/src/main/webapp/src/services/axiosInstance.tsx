7dbb49c7a287cdd4ca7bd21d2bf670f1bcb1b30a
Vigan Lladrovci <v.lladrovci@finway.de>
2022-07-18 12:51:41 +0200

    axios instanc to typescript

---
+++ clients/src/main/webapp/src/services/axiosInstance.tsx
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { NodeEnum } from "enums";
+
+const axiosFSA = axios.create({
+  baseURL: "http://localhost:10050",
+});
+
+const axiosBrainFinance = axios.create({
+  baseURL: "http://localhost:10051",
+});
+
+const axiosCapitalHolding = axios.create({
+  baseURL: "",
+});
+
+const axiosNotary = axios.create({
+  baseURL: "",
+});
+
+const axiosAuditor = axios.create({
+  baseURL: "",
+});
+
+const getAxiosInstance = (node: NodeEnum) => {
+  switch (node) {
+    case NodeEnum.SUPERVISORY_AUTHORITY:
+      return axiosFSA;
+    case NodeEnum.BRAIN_FINANCE:
+      return axiosBrainFinance;
+    case NodeEnum.CAPITALS_HOLDING:
+      return axiosCapitalHolding;
+    case NodeEnum.AUDITOR:
+      return axiosAuditor;
+    default:
+      return axiosFSA;
+  }
+};
+
+export { getAxiosInstance };
