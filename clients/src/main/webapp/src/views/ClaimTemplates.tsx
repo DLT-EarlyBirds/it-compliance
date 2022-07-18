8cde78d9d20d7cf514e7462ced8c78128003a643
Vigan Lladrovci <v.lladrovci@finway.de>
2022-07-18 15:28:34 +0200

    accept/reject claims UI

--- clients/src/main/webapp/src/views/ClaimTemplates.tsx
+++ clients/src/main/webapp/src/views/ClaimTemplates.tsx
@@ -1,8 +1,11 @@
 import React from "react";
-import { Table } from "antd";
+import { Table, Button } from "antd";
 import { useData } from "../contexts/DataContext";
+import { useNode } from "../contexts/NodeContext";
+import { NodeEnum } from "enums";
+import { Regulation } from "models";
 
-const columns = [
+const claimTemplateColumns = [
   {
     title: "Linear id",
     dataIndex: ["linearId", "id"],
@@ -21,12 +24,56 @@ const columns = [
   },
 ];
 
+const claimTemplateSuggestions = [
+  {
+    title: "Linear id",
+    dataIndex: ["linearId", "id"],
+  },
+  {
+    name: "Name",
+    dataIndex: "name",
+  },
+  {
+    title: "Template Description",
+    dataIndex: "templateDescription",
+  },
+  {
+    title: "Issuer",
+    dataIndex: "issuer",
+  },
+  {
+    title: "Actions",
+    dataIndex: ["linearId", "id"],
+    render: ({ linearId }: Regulation) => {
+      return (
+        <>
+          <Button onClick={() => console.log(linearId.id)}>Reject</Button>
+          <Button type="primary" onClick={() => console.log(linearId.id)}>
+            Accept
+          </Button>
+        </>
+      );
+    },
+  },
+];
+
 function ClaimTemplates() {
+  // TODO: Add claim template suggestions
   const { claimTemplates } = useData();
+  const { currentNode } = useNode();
 
   return (
     <div>
-      <Table columns={columns} dataSource={claimTemplates} />
+      {currentNode === NodeEnum.SUPERVISORY_AUTHORITY ? (
+        <h1>Create Claim Template</h1>
+      ) : (
+        <h1>Create Claim Template Suggestion</h1>
+      )}
+
+      {currentNode === NodeEnum.SUPERVISORY_AUTHORITY && (
+        <Table columns={claimTemplateSuggestions} dataSource={claimTemplates} />
+      )}
+      <Table columns={claimTemplateColumns} dataSource={claimTemplates} />
     </div>
   );
 }
