81fc12c098fe5e50135907a3e759e93b25335680
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-07-19 22:29:57 +0200

    Add get attachment endpoints to all clients

--- clients/src/main/java/com/compliance/auditor/controllers/SpecificClaimController.java
+++ clients/src/main/java/com/compliance/auditor/controllers/SpecificClaimController.java
@@ -9,13 +9,17 @@ import net.corda.core.node.services.Vault;
 import net.corda.core.node.services.vault.QueryCriteria;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -110,6 +114,28 @@ public class SpecificClaimController {
                     .collect(Collectors.toList());
             if (specificClaims.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
             else return ResponseEntity.status(HttpStatus.OK).body(specificClaims.get(0));
+        } else return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
+
+    @GetMapping("/attachment/{linearId}")
+    private ResponseEntity<InputStreamResource> openAttachment(@PathVariable String linearId) throws ExecutionException, InterruptedException, IOException {
+        UniqueIdentifier id = UniqueIdentifier.Companion.fromString(linearId);
+        QueryCriteria queryCriteria = new QueryCriteria.LinearStateQueryCriteria(
+                null,
+                Collections.singletonList(id),
+                Vault.StateStatus.UNCONSUMED,
+                Collections.singleton(SpecificClaim.class));
+        // Get state with linear id
+        List<SpecificClaim> specificClaims = proxy.vaultQueryByCriteria(queryCriteria, SpecificClaim.class).getStates().stream().map(
+                specificClaimStateAndRef -> specificClaimStateAndRef.getState().getData()
+        ).collect(Collectors.toList());
+        // Check if state with that linear ID exists
+        if (!specificClaims.isEmpty()) {
+            SpecificClaim specificClaim = specificClaims.get(0);
+            if (proxy.attachmentExists(specificClaim.getAttachmentID())) {
+                InputStreamResource file = new InputStreamResource(proxy.openAttachment(specificClaim.getAttachmentID()));
+                return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + specificClaim.getAttachmentID() + ".jar\"").body(file);
+            } else return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         } else return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
 }
