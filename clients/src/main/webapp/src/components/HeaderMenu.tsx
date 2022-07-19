ac7b36aef07ea1639a78f95a17e12c9c61d8da77
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-07-19 18:30:01 +0200

    added new logos to header

--- clients/src/main/webapp/src/components/HeaderMenu.tsx
+++ clients/src/main/webapp/src/components/HeaderMenu.tsx
@@ -1,9 +1,15 @@
 import React, {useState} from "react"
-import {Menu, Button, Drawer, Radio} from "antd"
-import {RadarChartOutlined, FileTextOutlined, OneToOneOutlined, HomeOutlined, FormOutlined} from "@ant-design/icons"
+import {Button, Drawer, Radio} from "antd"
 import {Link, useLocation} from "react-router-dom"
 import {useNode} from "../contexts/NodeContext"
 import {NodeEnum} from "enums"
+
+
+const BrainFinance = require('../assets/BrainFinance.png');
+const CapitalsHolding = require('../assets/CapitalsHolding.png');
+const Auditor = require('../assets/Auditor.png');
+const SupervisoryAuthority = require('../assets/SupervisoryAuthority.png');
+
 
 const HeaderMenu = () => {
     const {pathname} = useLocation()
@@ -25,12 +31,21 @@ const HeaderMenu = () => {
 
     return (
         <>
-            <div className='flex justify-between'>
-                <Menu className='justify-between w-1/4' theme={'dark'} mode="horizontal" selectedKeys={[pathname]} defaultSelectedKeys={["home"]}>
-                    <Menu.Item key="/" icon={<HomeOutlined />}>
-                        <Link to="/">Home</Link>
-                    </Menu.Item>
-                </Menu>
+            <div className='flex justify-between h-full'>
+                <Link to="/">
+                    {currentNode === NodeEnum.BRAIN_FINANCE && <
+                        img className='self-center m-2 h-14' src={BrainFinance}/>
+                    }
+                    {currentNode === NodeEnum.CAPITALS_HOLDING && <
+                        img className='self-center m-2 h-14' src={CapitalsHolding}/>
+                    }
+                    {currentNode === NodeEnum.SUPERVISORY_AUTHORITY && <
+                        img className='self-center m-2 h-14' src={SupervisoryAuthority}/>
+                    }
+                    {currentNode === NodeEnum.AUDITOR && <
+                        img className='self-center m-2 h-14' src={Auditor}/>
+                    }
+                </Link>
                 <div className="flex items-center px-5">
                     <h2 className="text-green-600 my-0">Node: {currentNode}</h2>
                     <Button className="mx-5" type="primary" onClick={showDrawer}>
@@ -39,7 +54,8 @@ const HeaderMenu = () => {
                 </div>
             </div>
 
-            <Drawer title="All nodes in the system" placement="bottom" closable={false} onClose={onClose} visible={visible} height="200">
+            <Drawer title="All nodes in the system" placement="bottom" closable={false} onClose={onClose}
+                    visible={visible} height="200">
                 <h2>You are currently connected to:</h2>
                 <Radio.Group defaultValue={currentNode} buttonStyle="solid" onChange={handleNodeChange}>
                     <Radio.Button value={NodeEnum.SUPERVISORY_AUTHORITY}>{NodeEnum.SUPERVISORY_AUTHORITY}</Radio.Button>
