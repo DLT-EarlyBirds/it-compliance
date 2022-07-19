cffaa64814c7bf748d05489d7b8599640e182daf
Vigan Lladrovci <v.lladrovci@finway.de>
2022-07-19 09:33:04 +0200

    Update claim templates

---
+++ clients/src/main/webapp/src/components/UpdateClaimTemplate.tsx
@@ -0,0 +1,91 @@
+import React from "react"
+import { Button, Form, Input, Drawer, Select } from "antd"
+import { ClaimTemplateDTO, Rule, ClaimTemplate } from "models"
+import { useNode } from "../contexts/NodeContext"
+import { useData } from "contexts/DataContext"
+import ClaimTemplateService from "services/ClaimTemplate.service"
+
+const { Option } = Select
+
+interface UpdateClaimTemplateProps {
+    claimTemplate: ClaimTemplate
+    isVisible: boolean
+    setIsVisible: (isVisible: boolean) => void
+}
+
+const UpdateClaimTemplate = ({ claimTemplate, isVisible, setIsVisible }: UpdateClaimTemplateProps) => {
+    const { axiosInstance } = useNode()
+    const { claimTemplates, setClaimTemplates, rules } = useData()
+
+    const onFinish = (values: ClaimTemplateDTO) => {
+        ClaimTemplateService.update(axiosInstance, { ...values, linearId: claimTemplate.linearId.id }).then((response) => {
+            const updatedClaimTemplates = claimTemplates.map((r) => (r.linearId.id === response.linearId.id ? response : r))
+            setClaimTemplates(updatedClaimTemplates)
+            setIsVisible(false)
+        })
+    }
+
+    return (
+        <Drawer title="Update Rule" placement="right" closable={false} onClose={() => setIsVisible(false)} visible={isVisible} height="200">
+            <Form
+                name="basic"
+                labelCol={{
+                    span: 8,
+                }}
+                onFinish={onFinish}
+            >
+                <Form.Item
+                    label="Name"
+                    name="name"
+                    initialValue={claimTemplate.name}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Template Description"
+                    name="templateDescription"
+                    initialValue={claimTemplate.templateDescription}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Rule"
+                    name="rule"
+                    initialValue={claimTemplate.rule}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Select
+                        style={{
+                            width: 120,
+                        }}
+                    >
+                        {rules.map((rule: Rule) => (
+                            <Option value={rule.linearId.id}>{rule.name}</Option>
+                        ))}
+                    </Select>
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+        </Drawer>
+    )
+}
+
+export default UpdateClaimTemplate
--- clients/src/main/webapp/src/views/ClaimTemplates.tsx
+++ clients/src/main/webapp/src/views/ClaimTemplates.tsx
@@ -1,29 +1,12 @@
-import React from "react"
+import React, { useState } from "react"
 import { Table, Button } from "antd"
 import { useData } from "../contexts/DataContext"
 import { useNode } from "../contexts/NodeContext"
 import { NodeEnum } from "enums"
-import { Regulation } from "models"
+import { ClaimTemplate, Regulation } from "models"
 import CreateClaimTemplate from "../components/CreateClaimTemplate"
-
-const claimTemplateColumns = [
-    {
-        title: "Linear id",
-        dataIndex: ["linearId", "id"],
-    },
-    {
-        title: "Name",
-        dataIndex: "name",
-    },
-    {
-        title: "Template Description",
-        dataIndex: "templateDescription",
-    },
-    {
-        title: "Issuer",
-        dateIndex: "issuer",
-    },
-]
+import { EditOutlined } from "@ant-design/icons"
+import UpdateClaimTemplate from "components/UpdateClaimTemplate"
 
 const claimTemplateSuggestions = [
     {
@@ -62,6 +45,43 @@ function ClaimTemplates() {
     // TODO: Add claim template suggestions
     const { claimTemplates } = useData()
     const { currentNode } = useNode()
+    const [isDrawerVisible, setIsDrawerVisible] = useState(false)
+    const [claimTemplate, setCurrentClaimTemplate] = useState<ClaimTemplate | undefined>(undefined)
+
+    const claimTemplateColumns = [
+        {
+            title: "Linear id",
+            dataIndex: ["linearId", "id"],
+        },
+        {
+            title: "Name",
+            dataIndex: "name",
+        },
+        {
+            title: "Template Description",
+            dataIndex: "templateDescription",
+        },
+        {
+            title: "Issuer",
+            dateIndex: "issuer",
+        },
+        {
+            title: "Actions",
+            render: (_: string, claimTemplate: ClaimTemplate) => {
+                return (
+                    <Button
+                        type="primary"
+                        onClick={() => {
+                            setIsDrawerVisible(true)
+                            setCurrentClaimTemplate(claimTemplate)
+                        }}
+                    >
+                        <EditOutlined />
+                    </Button>
+                )
+            },
+        },
+    ]
 
     return (
         <div>
@@ -69,6 +89,7 @@ function ClaimTemplates() {
 
             {currentNode === NodeEnum.SUPERVISORY_AUTHORITY && <Table columns={claimTemplateSuggestions} dataSource={claimTemplates} />}
             <Table columns={claimTemplateColumns} dataSource={claimTemplates} />
+            {isDrawerVisible && <UpdateClaimTemplate claimTemplate={claimTemplate as ClaimTemplate} isVisible={isDrawerVisible} setIsVisible={setIsDrawerVisible} />}
         </div>
     )
 }
